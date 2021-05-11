f761df8c286d7741830eedfd1266d0136a87d554
Robert Ramirez <metaknight757@gmail.com>
2021-05-10 23:05:46 -0500

    added new file to get posts and renamed the file in components

--- client/src/PostService.js
+++ client/src/PostService.js
@@ -4,8 +4,33 @@ const url = "http://localhost:5000/api/posts";
 
 class PostService {
     //Get Posts
-
+    static getPosts() {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const res = await axios.get(url);
+                const data = res.data;
+                resolve(
+                    data.map(posts => ({
+                        ...posts,
+                        createdAt: new Date(posts.createdAt)
+                    }))
+                );
+            } catch (err) {
+                reject(err);
+            }
+        });
+    }
     //Create Posts
+    static insertPost(text) {
+        return axios.post(url, {
+            text
+        });
+    }
 
     //Delete Posts
+    static deletePost(id) {
+        return axios.delete(`${url}${id}`)
+    }
 }
+
+export default PostService;
